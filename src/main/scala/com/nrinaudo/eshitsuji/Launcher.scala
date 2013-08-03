ad4cb696c9d71935dfc3cf17819f604212bdc577
Nicolas Rinaudo <nicolas@nrinaudo.com>
2013-08-03 16:29:07 +0200

    Safer initialisation of the TwitterNotifier, defaulting to STDOUT notification.

--- src/main/scala/com/nrinaudo/eshitsuji/Launcher.scala
+++ src/main/scala/com/nrinaudo/eshitsuji/Launcher.scala
@@ -3,20 +3,22 @@ package com.nrinaudo.eshitsuji
 import com.nrinaudo.eshitsuji.storage.Storage
 
 object Launcher extends App {
-  val notifier = new notif.StdoutNotifier()
+  // Initialises storage and notification
   val storage  = Storage()
+  val notifier = notif.TwitterNotifier(storage.conf) getOrElse new notif.StdoutNotifier()
 
-  val authors = new amazon.AuthorMonitor(storage, notifier)
+  // Initialises monitors.
+  val authors    = new amazon.AuthorMonitor(storage, notifier)
   val publishers = new apple.PublisherMonitor(java.util.Locale.FRENCH, storage, notifier)
 
+  // Initialises web UI
   val plan = new web.AdminPlan(storage)
-
   plan.register("amazon", authors)
   plan.register("apple",  publishers)
 
+  // Starts all services.
   notifier.start()
   authors.start()
   publishers.start()
-
   unfiltered.jetty.Http.local(8080).filter(plan).run()
 }
--- src/main/scala/com/nrinaudo/eshitsuji/notif/TwitterNotifier.scala
+++ src/main/scala/com/nrinaudo/eshitsuji/notif/TwitterNotifier.scala
@@ -43,19 +43,25 @@ class TwitterNotifier(private val service: OAuthService, private val token: Toke
   */
 object TwitterNotifier {
   /** Name of the configuration variable that contains the Twitter service key to use. */
-  val ServiceKey       = "twitter.service.key"
+  val ServiceKey    = "twitter.service.key"
   /** Name of the configuration variable that contains the Twitter service secret to use. */
-  val ServiceSecretKey = "twitter.service.secret"
+  val ServiceSecret = "twitter.service.secret"
   /** Name of the configuration variable that contains the Twitter token to use. */
-  val TokenValueKey    = "twitter.token.value"
+  val TokenValue    = "twitter.token.value"
   /** Name of the configuration variable that contains the Twitter token secret to use. */
-  val TokenSecretKey   = "twitter.token.secret"
+  val TokenSecret   = "twitter.token.secret"
 
-  def apply(conf: Configuration) = new TwitterNotifier(service(conf), token(conf))
+  def apply(conf: Configuration): Option[TwitterNotifier] = service(conf) flatMap {s =>
+    token(conf) map {t => new TwitterNotifier(s, t)}
+  }
 
-  def service(conf: Configuration): OAuthService = service(conf(ServiceKey), conf(ServiceSecretKey))
+  def service(conf: Configuration): Option[OAuthService] = conf.get(ServiceKey) flatMap {key =>
+    conf.get(ServiceSecret) map {secret => service(key, secret)}
+  }
 
-  def token(conf: Configuration): Token = token(conf(TokenValueKey), conf(TokenSecretKey))
+  def token(conf: Configuration): Option[Token] =  conf.get(TokenValue) flatMap {value =>
+    conf.get(TokenSecret) map {secret => token(value, secret)}
+  }
 
   /** Creates an instance of `Token` for the specified value and secret. */
   def token(token: String, secret: String): Token = new Token(token, secret)
